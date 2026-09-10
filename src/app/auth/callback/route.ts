353e755ebdddf840e0b9b0feff53a3d251309914
vzarat <vzarat96@gmail.com>
2026-09-10 13:35:38 -0500

    google fix 2

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
   }
 
   try {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
 
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
